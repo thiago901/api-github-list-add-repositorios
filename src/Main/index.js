bcde180eda527e68581bdb909768e08a4680d34d
Thiago Santos <tsrocha901@gmail.com>
2020-02-18 08:36:32 -0300

    add styled, init components estilizados

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { FaGithub, FaPlus } from 'react-icons/fa';
+import { Container, Form, ButtonSubmit } from './style';
 
 function Main() {
-    return <h1>Main</h1>;
+    return (
+        <Container>
+            <h1>
+                <FaGithub />
+                Repositórios
+            </h1>
+
+            <Form onSubimit={() => {}}>
+                <input type="text" placeholder="Adicionar reposiório" />
+            </Form>
+
+            <ButtonSubmit disable>
+                <FaPlus color="#FFF" size={14} />
+            </ButtonSubmit>
+        </Container>
+    );
 }
 
 export default Main;
---
+++ src/Main/style.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+export const Form = styled.form``;
+export const ButtonSubmit = styled.button``;
